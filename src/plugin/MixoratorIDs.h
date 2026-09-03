09a689e5199764ab6847e7a00b27da296d7c70ae
challanger2000 <challanger2000@gmx.de>
2026-09-03 13:27:39 +0200

    Add stable VST3 class IDs

---
+++ src/plugin/MixoratorIDs.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "pluginterfaces/base/funknown.h"
+
+namespace Mixorator
+{
+static const Steinberg::FUID kProcessorUID(0x4D69786F, 0x7261746F, 0x72000000, 0x00000001);
+static const Steinberg::FUID kControllerUID(0x4D69786F, 0x7261746F, 0x72000000, 0x00000002);
+}
