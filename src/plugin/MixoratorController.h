4187d829aca772920b4fee3d57ca9674c741c402
challanger2000 <challanger2000@gmx.de>
2026-09-05 04:08:26 +0200

    Add non-automatable GUI interaction state

--- src/plugin/MixoratorController.h
+++ src/plugin/MixoratorController.h
@@ -2,14 +2,25 @@
 
 #include "public.sdk/source/vst/vsteditcontroller.h"
 #include "public.sdk/source/vst/utility/dataexchange.h"
+#include "vstgui/plugin-bindings/vst3editor.h"
+#include "vstgui/lib/controls/icontrollistener.h"
 #include "AnalysisExchange.h"
 
 #include <cstdint>
 
+namespace VSTGUI
+{
+class CControl;
+class COptionMenu;
+class CTextLabel;
+}
+
 namespace Mixorator
 {
 class Controller : public Steinberg::Vst::EditController,
-                   public Steinberg::Vst::IDataExchangeReceiver
+                   public Steinberg::Vst::IDataExchangeReceiver,
+                   public VSTGUI::VST3EditorDelegate,
+                   public VSTGUI::IControlListener
 {
 public:
     OBJ_METHODS(Controller, Steinberg::Vst::EditController)
@@ -55,15 +66,71 @@ public:
         Analysis::Genre genre,
         Analysis::Era era) const noexcept;
 
+    // VSTGUI delegate / local controls. These controls are deliberately not VST parameters,
+    // so Studio One cannot record them as automation lanes.
+    VSTGUI::CView* verifyView(
+        VSTGUI::CView* view,
+        const VSTGUI::UIAttributes& attributes,
+        const VSTGUI::IUIDescription* description,
+        VSTGUI::VST3Editor* editor) override;
+    void didOpen(VSTGUI::VST3Editor* editor) override;
+    void willClose(VSTGUI::VST3Editor* editor) override;
+    void valueChanged(VSTGUI::CControl* control) override;
+
 private:
+    enum UiTag : std::int32_t
+    {
+        kUiMix = 10001,
+        kUiMaster = 10002,
+        kUiLive = 10003,
+        kUiFinal = 10004,
+        kUiGenre = 10005,
+        kUiEra = 10006,
+        kUiReset = 10007,
+        kUiAnalyze = 10008
+    };
+
     Steinberg::tresult requestAnalysisState(Steinberg::int64 state) noexcept;
     void requestFinalSnapshot(std::uint64_t generation) noexcept;
     bool consumeFinalSnapshotMessage(Steinberg::Vst::IMessage* message) noexcept;
+
+    void refreshUi() noexcept;
+    void clearUiPointers() noexcept;
+    void bindNamedView(VSTGUI::CView* view, const VSTGUI::UIAttributes& attributes) noexcept;
+    void updateSelectionControls() noexcept;
 
     Steinberg::Vst::DataExchangeReceiverHandler dataExchange_ {this};
     AnalysisExchangePacket latestPacket_ {};
     bool hasPacket_ {false};
     std::uint64_t requestedFinalGeneration_ {0};
     std::uint64_t finalSnapshotGeneration_ {0};
+
+    Analysis::AnalysisMode uiMode_ {Analysis::AnalysisMode::Mix};
+    Analysis::Genre uiGenre_ {Analysis::Genre::General};
+    Analysis::Era uiEra_ {Analysis::Era::Modern};
+    bool uiFinalSelected_ {false};
+
+    VSTGUI::VST3Editor* editor_ {nullptr};
+    VSTGUI::CControl* mixControl_ {nullptr};
+    VSTGUI::CControl* masterControl_ {nullptr};
+    VSTGUI::CControl* liveControl_ {nullptr};
+    VSTGUI::CControl* finalControl_ {nullptr};
+    VSTGUI::COptionMenu* genreMenu_ {nullptr};
+    VSTGUI::COptionMenu* eraMenu_ {nullptr};
+
+    VSTGUI::CTextLabel* technicalVerdict_ {nullptr};
+    VSTGUI::CTextLabel* styleVerdict_ {nullptr};
+    VSTGUI::CTextLabel* pcmVerdict_ {nullptr};
+    VSTGUI::CTextLabel* streamingVerdict_ {nullptr};
+    VSTGUI::CTextLabel* stateLabel_ {nullptr};
+    VSTGUI::CTextLabel* overallVerdict_ {nullptr};
+    VSTGUI::CTextLabel* overallLine1_ {nullptr};
+    VSTGUI::CTextLabel* overallLine2_ {nullptr};
+    VSTGUI::CTextLabel* integratedValue_ {nullptr};
+    VSTGUI::CTextLabel* truePeakValue_ {nullptr};
+    VSTGUI::CTextLabel* plrValue_ {nullptr};
+    VSTGUI::CTextLabel* lraValue_ {nullptr};
+    VSTGUI::CTextLabel* correlationValue_ {nullptr};
+    VSTGUI::CTextLabel* monoValue_ {nullptr};
 };
 }
